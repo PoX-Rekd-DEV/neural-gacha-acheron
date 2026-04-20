a3b82d3d3e66b40d9798f7864c5292b3ec504d9d
PoX-Rekd-DEV <2525jddj2525@gmail.com>
2026-04-20 17:34:35 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const path = require('path'); // Added this for path safety
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 450,
-        height: 800,
-        resizable: false, // Keeps the layout from breaking
-        autoHideMenuBar: true, // Hides the "File/Edit" bar for immersion
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+  const win = new BrowserWindow({
+    width: 450,
+    height: 800,
+    resizable: false,
+    autoHideMenuBar: true, // Clean look
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
 
-    win.loadFile('index.html');
+  // This line is the "Fix": it tells Electron to look in the
+  // exact folder where the script is running.
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
 
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
